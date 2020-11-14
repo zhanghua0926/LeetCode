10ec90e08901c27d993a6a8729cf07936337ffde
Eric <zh19920926@live.com>
2020-11-14 11:17:30 +0800

    two sum algorithm

---
+++ Algorithm/Algorithm/Program.cs
@@ -0,0 +1,24 @@
+﻿using Algorithm.SumOfTwoNumAlgorithm;
+using System;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            GetSumOfTwoNum();
+        }
+
+        private static void GetSumOfTwoNum()
+        {
+            int[] nums = new int[] { 2, 7, 7, 11, 15 };
+            foreach (var item in TwoSumSolution.TwoSum(nums, 22))
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Algorithm/Algorithm/TwoSumAlgorithm/TwoSumSolution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithm.SumOfTwoNumAlgorithm
+{
+    public static class TwoSumSolution
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+                if (dict.ContainsKey(complement))
+                {
+                    return new int[] { dict[complement], i };
+                }
+
+                if (!dict.ContainsKey(nums[i]))
+                    dict.Add(nums[i], i);
+            }
+
+            throw new InvalidOperationException("No two sum solution");
+        }
+    }
+}
