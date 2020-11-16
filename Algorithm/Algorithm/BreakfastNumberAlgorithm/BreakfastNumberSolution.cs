34e70334fdbfbe8946ac48df414d02acadd603f2
Eric <zh19920926@live.com>
2020-11-16 22:37:38 +0800

    two sum

---
+++ Algorithm/Algorithm/BreakfastNumberAlgorithm/BreakfastNumberSolution.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.BreakfastNumberAlgorithm
+{
+    public static class BreakfastNumberSolution
+    {
+    }
+}
